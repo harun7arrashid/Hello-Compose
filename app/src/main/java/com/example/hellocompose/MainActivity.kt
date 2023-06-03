f968e57d426e64af8b1fc1038d55e7fa5582a891
Harun AR-Rashid <harun4arrashid@gmail.com>
2023-06-03 14:32:54 +0700

    animation spring

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -11,6 +14,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.outlined.ExpandMore
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -78,7 +83,14 @@ fun GreetingList(names: List<String>) {
 @Composable
 fun Greeting(name: String) {
 
-    var isExpanded = remember { mutableStateOf(false) } // untuk menjaga supaya nilai tersebut tidak terreset ketika update UI.
+    var isExpanded by remember { mutableStateOf(false) } // untuk menjaga supaya nilai tersebut tidak terreset ketika update UI.
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Row(
         modifier = Modifier.padding(8.dp),
@@ -88,7 +100,7 @@ fun Greeting(name: String) {
         Image(
             painter = painterResource(id = R.drawable.jetpack_compose),
             contentDescription = "Logo Jetpack Compose",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.size(animatedSizeDp)
         )
         
         Spacer(modifier = Modifier.width(8.dp))
@@ -102,10 +114,10 @@ fun Greeting(name: String) {
             Text(text = "Welcome Dicoding")
         }
         
-        IconButton(onClick = { isExpanded.value = !isExpanded.value  }) { // for trigger animation
+        IconButton(onClick = { isExpanded = !isExpanded }) { // for trigger animation
             Icon(
-                imageVector = if (isExpanded.value) Icons.Filled.ExpandLess else Icons.Outlined.ExpandMore,
-                contentDescription = if (isExpanded.value) "Show less" else "Show More"
+                imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Outlined.ExpandMore,
+                contentDescription = if (isExpanded) "Show less" else "Show More"
             )
         }
     }
